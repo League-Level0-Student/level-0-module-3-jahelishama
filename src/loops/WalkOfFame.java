36f489a7383ba056571867500fdf13fec449c9f0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-23 18:41:38 -0800

    walk of fame

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -7,15 +7,76 @@ package loops;
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
-	public static void main(String[] args) {
+			for (int i = 0; i < 5; i++) {
+		rob.move(30);
+		rob.turn(144);
+		 static void main(String[] args) {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(0);
+		rob.setSpeed(100);
+		rob.penDown();
 		// 2. Make the robot draw a star shape. Hint: 144.
+		for (int i = 0; i < 5; i++) {
+			rob.move(30);
+			rob.turn(144);
+			
+		}
+		rob.setX(50);
+		
+		for (int i = 0; i < 5; i++) {
+			rob.move(30);
+			rob.turn(144);
+			
+		}
+		rob.hide();
+//	    Copyright (c) The League of Amazing Programmers 2013-2017
+
+		package loops;
+
+		import org.jointheleague.graphical.robot.Robot;
+
+		public class WalkOfFame {
+			publi		for (int i = 0; i < 5; i++) {
+				rob.move(30);
+				rob.turn(144);
+				c static void main(String[] args) {
+				Robot rob = new Robot();
+				// 1. Set the X position of the robot so that it starts on the left. 
+				// You also need to show the robot to see the result of this line.
+				rob.setX(0);
+				rob.setSpeed(100);
+				rob.penDown();
+				// 2. Make the robot draw a star shape. Hint: 144.
+				for (int i = 0; i < 5; i++) {
+					rob.move(30);
+					rob.turn(144);
+					
+				}
+				rob.setX(50);
+				
+				for (int i = 0; i < 5; i++) {
+					rob.move(30);
+					rob.turn(144);
+					
+				}
+				rob.hide();
+				
+				// 3. Set the size of the star to 30.
+				/** THE CHALLENGE: **/
+				/*
+				 * Make the robot draw a line of stars like this:
+				 * http://bit.ly/walk-of-fame
+				 * 
+				 * Hint: The distance between stars is 50.
+				 */
+
+			}
+
+		}
 
 		// 3. Set the size of the star to 30.
-
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
