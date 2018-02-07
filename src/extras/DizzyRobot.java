c56256e3db6de2cd7751034200e53aa59866827e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-06 19:19:52 -0800

    Dizzy robot

--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -3,6 +3,8 @@
 
 package extras;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
@@ -10,8 +12,11 @@ public class DizzyRobot {
 	public static void main(String[] args) {
 		
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-
+	String fullHouse=JOptionPane.showInputDialog("how dizzy are you");
+	int trains=Integer.parseInt(fullHouse);		
  // 1. Use the dance method to make the robot spin.
+	dance(trains);
+		
 	
 
 	}
